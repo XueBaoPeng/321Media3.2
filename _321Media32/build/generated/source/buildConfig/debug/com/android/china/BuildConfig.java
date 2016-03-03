7a68f7a7ad6b214c37b86d470c8675626e7464be
XueBaoPeng <13519297683@163.com>
2016-03-03 10:04:45 +0800

    Initial commit

---
+++ _321Media32/build/generated/source/buildConfig/debug/com/android/china/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.android.china;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.android.china";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 14;
+  public static final String VERSION_NAME = "";
+}
